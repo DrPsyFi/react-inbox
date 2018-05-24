98546cbcd377189f04af8d266f4c65b8d0d33384
Sunil Kakar <26909769+DrPsyFi@users.noreply.github.com>
2018-05-23 20:03:56 -0700

    toolbar partially completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Messages from './components/Messages.js'
 import './App.css';
 import data from './data.js'
 
+data.forEach(message => {
+  if (message.selected) {
+
+  }
+  else {
+    message.selected = false;
+  }
+})
+
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -16,15 +26,37 @@ class App extends Component {
     this.setState({ data: this.state.data.map(message => message.id === id ? {...message, starred : !message.starred} : {...message})  })
   }
   toggleCheck= (id) => {
-  
+
     this.setState({ data: this.state.data.map(message => message.id === id ? {...message, selected : !message.selected} : {...message})  })
   }
+  setSelectedMessagesToRead= () => {
+    this.setState({ data: this.state.data.map(message => message.selected ? {...message, read: true} : {...message} )})
+  }
   render() {
+
+    let unReadCount = 0;
+    let data = this.state.data
+     for(let i = 0; i < data.length; i++) {
+       if(!data[i].read) {
+         unReadCount+= 1
+       }
+       console.log(unReadCount)
+     }
+
+
     return (<div className="App">
       <div className="container">
 
-        <Toolbar/>
-        <Messages messages={this.state.data} toggleStar={this.toggleStar} toggleCheck={this.toggleCheck} />
+        <Toolbar
+           allChecked={this.state.data.reduce((acc, ele) => acc && ele.selected, true) }
+           noneChecked={this.state.data.reduce((acc, ele) => acc && !ele.selected, true)}
+           unReadCount={unReadCount}
+           setSelectedMessagesToRead={this.setSelectedMessagesToRead}
+        />
+        <Messages messages={this.state.data}
+           toggleStar={this.toggleStar}
+           toggleCheck={this.toggleCheck}
+         />
 
       </div>
     </div>)
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,42 +1,62 @@
 //1. Import from React
 import React from "react"
 //2.Create a function
-const Toolbar = (props) => {
+
+////const Toolbar = (props) =>
+///Below is an example of destructuring props
+
+const Toolbar = ({allChecked, noneChecked, setSelectedMessagesToRead, unReadCount}) => {
+  let checkClass
+
+    if (allChecked) {
+      checkClass= "fa-check-square-o"
+    }
+    else if (noneChecked) {
+      checkClass= "fa-square-o"
+    }
+    else {
+      checkClass= "fa-minus-square-o"
+    }
+
+
     return (
     <div className="row toolbar">
       <div className="col-md-12">
         <p className="pull-right">
-          <span className="badge badge">2</span>
+          <span className="badge badge">{unReadCount}</span>
           unread messages
         </p>
 
         <button className="btn btn-default">
-          <i className="fa fa-check-square-o"></i>
+          <i className={`fa ${checkClass}`}></i>
         </button>
 
-        <button className="btn btn-default">
+        <button className="btn btn-default"
+        disabled={noneChecked}
+        onClick={setSelectedMessagesToRead}
+        >
           Mark As Read
         </button>
 
-        <button className="btn btn-default">
+        <button className="btn btn-default" disabled={noneChecked}>
           Mark As Unread
         </button>
 
-        <select className="form-control label-select">
+        <select className="form-control label-select" disabled={noneChecked} >
           <option>Apply label</option>
           <option value="dev">dev</option>
           <option value="personal">personal</option>
           <option value="gschool">gschool</option>
         </select>
 
-        <select className="form-control label-select">
+        <select className="form-control label-select" disabled={noneChecked}>
           <option>Remove label</option>
           <option value="dev">dev</option>
           <option value="personal">personal</option>
           <option value="gschool">gschool</option>
         </select>
 
-        <button className="btn btn-default">
+        <button className="btn btn-default" disabled={noneChecked}>
           <i className="fa fa-trash-o"></i>
         </button>
       </div>
